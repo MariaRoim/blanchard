b50c8c95f77fecf709b9f9d88857caa4f3c25bf1
Maria Roim <masha.roim@gmail.com>
2023-04-07 12:04:17 +0800

    add section contacts

--- js/script.js
+++ js/script.js
@@ -146,3 +146,62 @@ function f_acc(){
       prevEl: '.swiper-button-prev',
       },
   });
+
+  var element = document.querySelector("input[type='tel']")
+  var im = new Inputmask("+7 (999) 999-99-99");
+  im.mask(element);
+
+  new JustValidate('.contacts__form', {
+    rules: {
+      name: {
+        required: true,
+        minLength: 2,
+        maxLength: 20
+      },
+  
+      tel: {
+        required: true,
+        function: (name, value) => {
+          const phone = element.inputmask.unmaskedvalue()
+          return Number(phone) && phone.length === 10
+        }
+    }
+    },
+  
+  messages: {
+    name: {
+      required: 'Недопустимый формат',
+    },
+  
+    tel: {
+      required: 'Недопустимый формат'
+    }
+  }
+  });
+
+  // Функция ymaps.ready() будет вызвана, когда
+    // загрузятся все компоненты API, а также когда будет готово DOM-дерево.
+    ymaps.ready(init);
+    function init(){
+        // Создание карты.
+        var myMap = new ymaps.Map("map", {
+            // Координаты центра карты.
+            // Порядок по умолчанию: «широта, долгота».
+            // Чтобы не определять координаты центра карты вручную,
+            // воспользуйтесь инструментом Определение координат.
+            center: [55.75846806898367,37.60108849999989],
+            // Уровень масштабирования. Допустимые значения:
+            // от 0 (весь мир) до 19.
+            zoom: 14
+        });
+        myMap.behaviors.disable('scrollZoom');
+
+var myPlacemark = new ymaps.Placemark([55.75846806898367,37.60108849999989], {}, {
+  iconLayout: 'default#image',
+  iconImageHref: 'img/place-mark.svg',
+  iconImageSize: [20, 20],
+});
+
+// Размещение геообъекта на карте.
+myMap.geoObjects.add(myPlacemark);
+    }
